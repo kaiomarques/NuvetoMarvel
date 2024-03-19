010e980b197544e8faa7ba80ce605edf219a5402
Kaio Luiz Marques <kaio.marques@bravocorp.com.br>
2024-03-19 20:41:24 -0300

    PSR12 na pasta Library

--- app/Library/PaginationTrait.php
+++ app/Library/PaginationTrait.php
@@ -4,11 +4,13 @@ namespace App\Library;
 
 trait PaginationTrait
 {
-    private function offset($currentPage, $perPage) {
+    private function offset($currentPage, $perPage)
+    {
         return ($currentPage - 1) * $perPage;
     }
 
-    private function totalPages($totalRecords, $perPage) {
+    private function totalPages($totalRecords, $perPage)
+    {
         return ceil($totalRecords / $perPage);
     }
 }
