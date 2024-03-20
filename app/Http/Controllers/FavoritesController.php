aab84efbc5641522afedb38b2db886e784d42814
Kaio Luiz Marques <kaio.marques@bravocorp.com.br>
2024-03-20 01:47:54 -0300

    Ajuste de phpdocs

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -69,8 +69,10 @@ class FavoritesController extends Controller
                 ->pluck('id_character')
                 ->toArray();
 
-            $comics = $this->comicService->getAllComics(100,0); //limite de busca na plataforma
-            $characters = $this->charactersService->getAllCharacters(100,0);//limite de busca na plataforma
+            $comics = $this->comicService->getAllComics(100,0); 
+            //limite de busca na plataforma
+            $characters = $this->charactersService->getAllCharacters(100,0);
+            //limite de busca na plataforma
 
             $favoriteComicsData = $this->_filterFavorites(
                 $comics['dados'], $favoriteComics
