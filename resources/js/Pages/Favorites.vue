19e8e4a504b1d9ab21089c1b6dc5bdae67fb18f6
Kaio Luiz Marques <kaio.marques@bravocorp.com.br>
2024-03-20 01:10:43 -0300

    Acertando bug

--- resources/js/Pages/Favorites.vue
+++ resources/js/Pages/Favorites.vue
@@ -4,7 +4,7 @@
             <h3>Favoritos</h3>
             <h4>Quadrinhos</h4>
             <div class="q-gutter-y-md" style="max-width: 600px">
-                <div class="comic-container" v-if="favoriteComics.length > 0">
+                <div class="comic-container" v-if="favoriteComics.length != 0">
                     <div class="comic" v-for="comic in favoriteComics">
                         <img class="imagem" :src="comic.image" />
                         <span>{{ comic.title }}</span>
@@ -18,7 +18,7 @@
             <div class="q-gutter-y-md" style="max-width: 600px">
                 <div
                     class="character-container"
-                    v-if="favoriteCharacters.length > 0"
+                    v-if="favoriteCharacters.length != 0"
                 >
                     <div
                         class="character"
