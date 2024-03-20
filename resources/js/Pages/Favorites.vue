c6b8a23148496562a2a476351e98cb633ecb8dd0
Kaio Luiz Marques <kaio.marques@bravocorp.com.br>
2024-03-20 01:06:05 -0300

    Label caso nenhum quadrinho ou personagem favoritos

--- resources/js/Pages/Favorites.vue
+++ resources/js/Pages/Favorites.vue
@@ -4,16 +4,22 @@
             <h3>Favoritos</h3>
             <h4>Quadrinhos</h4>
             <div class="q-gutter-y-md" style="max-width: 600px">
-                <div class="comic-container">
+                <div class="comic-container" v-if="favoriteComics.length > 0">
                     <div class="comic" v-for="comic in favoriteComics">
                         <img class="imagem" :src="comic.image" />
                         <span>{{ comic.title }}</span>
                     </div>
                 </div>
+                <div class="comic-container" v-if="favoriteComics.length == 0">
+                    <h6>Nenhum quadrinho favorito no momento</h6>
+                </div>
             </div>
             <h4>Personagens</h4>
             <div class="q-gutter-y-md" style="max-width: 600px">
-                <div class="character-container">
+                <div
+                    class="character-container"
+                    v-if="favoriteCharacters.length > 0"
+                >
                     <div
                         class="character"
                         v-for="character in favoriteCharacters"
@@ -21,6 +27,12 @@
                         <img class="imagem" :src="character.image" />
                         <span>{{ character.name }}</span>
                     </div>
+                </div>
+                <div
+                    class="comic-container"
+                    v-if="favoriteCharacters.length == 0"
+                >
+                    <h6>Nenhum personagem favorito no momento</h6>
                 </div>
             </div>
         </div>
